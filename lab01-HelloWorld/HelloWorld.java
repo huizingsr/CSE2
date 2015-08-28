9edb4edb2d5f9b86f9ffbe5482e088e8d84bce2a
huizingsr <srh217@lehigh.edu>
2015-08-28 21:00:10 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+///////////////////////
+//Sicco Huizing
+//Demo 0
+//Hello Wolrd Java Program
+//
+// first compile the program
+//      javac HelloWorld.java
+// run the program
+//      java HelloWorld//
+
+//define a class
+public class HelloWorld{
+    
+// add main method
+    public static void main(String [] args){
+        // print the statement with the pox around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
