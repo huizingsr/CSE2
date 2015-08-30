7da5005797644f85c71fc7134960ee00ce5c4b9e
huizingsr <srh217@lehigh.edu>
2015-08-30 23:24:40 +0000

    updated Welcome Class

---
+++ hw-01/WelcomeClass.java
@@ -0,0 +1,16 @@
+public class WelcomeClass{ //define class
+public static void main (String [] args){ //define method
+
+
+System.out.println("  ---------- ");
+System.out.println("  |Welcome|  ");
+System.out.println("  ---------- ");
+System.out.println("  ^  ^  ^  ^  ^  ^  ");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\  ");
+System.out.println("|-s--r--h--2--1--7-|  ");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /   ");
+System.out.println("  v  v  v  v  v  v      ");
+System.out.println(" This Summer I worked for IBM GBS in New York City");
+  
+}
+}
