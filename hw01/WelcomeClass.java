2be701d9c24babaaa8f86face1fd9acdc166b4f3
huizingsr <srh217@lehigh.edu>
2015-09-07 00:11:01 +0000

    updated HelloWorld

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,17 @@
+public class WelcomeClass{ //define class
+public static void main (String [] args){ //define method
+
+
+System.out.println("  ---------- "); //print out indivdual lines
+System.out.println("  |Welcome|  ");
+System.out.println("  ---------- ");
+System.out.println("  ^  ^  ^  ^  ^  ^  ");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\  "); //double slash necessary for spacing
+System.out.println("|-s--r--h--2--1--7-|  ");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /   ");
+System.out.println("  v  v  v  v  v  v      ");
+System.out.println(" This Summer I worked for IBM GBS in New York City");
+ 
+
+}
+}
